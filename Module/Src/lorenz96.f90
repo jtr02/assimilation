7690c22800d996e717fc822596758cdf288c36cd
jtr <jtr.1990.moff@gmail.com>
2018-09-15 17:35:03 +0900

    modify adams-bashforth scheme

--- Module/Src/lorenz96.f90
+++ Module/Src/lorenz96.f90
@@ -8,13 +8,15 @@ program lorenz96
   real, parameter    :: forcing = 2.0d0
 
   ! finite difference scheme
-  ! forward euler  : 1
-  ! leapflog       : 2
-  ! adams-bashford : 3
-  ! runge-kutta    : 4
-  ! matsuno        : 5
-  ! semi-implicit  : 6
-  integer, parameter :: fds = 2
+  ! forward-euler_1   : 1,1
+  ! leapfrog_2        : 2,2
+  ! adams-bashforth_2 : 3,2
+  ! adams-bashforth_3 : 4,3
+  ! adams-bashforth_4 : 5.4
+  ! runge-kutta_2     : 6,2
+  ! runge-kutta_3     : 7,3
+  ! runge-kutta_4     : 8,4
+  integer, parameter :: fds(2) = (/ 3 , 2 /)
 
   ! variable
   real, allocatable :: f(:, :)
@@ -29,7 +31,7 @@ program lorenz96
   write(*,*) 'integration time = ',nt
   write(*,*) 'time step   =      ',dt
   write(*,*) 'forcing     =      ',forcing
-  write(*,*) 'finite difference scheme = ',fds
+  write(*,*) 'finite difference scheme = ',fds(1)
 
   ! input initial condition
   allocate(f(nx+2, nt+1))
@@ -45,16 +47,22 @@ program lorenz96
 
   do t = 1, nt
 
-     call set_boundary_condition(nx, f(0:nx+1, t))
+     call set_boundary_condition(nx, f(0:nx+1,t))
 
-     if(t.eq.1) then
-        call forward_euler_scheme(nx, dt, forcing, f(0:nx+1, t), f(0:nx+1, t+1))
-     else
-        if(fds.eq.1) then
-           call forward_euler_scheme(nx, dt, forcing, f(0:nx+1, t), f(0:nx+1, t+1))
-        else if(fds.eq.2) then
-           call centered_scheme(nx, dt, forcing, f(0:nx+1, t), f(0:nx+1, t-1), f(0:nx+1, t+1))
-        end if
+     if(fds(1).eq.1) then
+        call forward_euler(nx, dt, forcing, f(0:nx+1,t), f(0:nx+1,t+1))
+     else if(t.lt.fds(2)) then
+        call forward_euler(nx, dt, forcing, f(0:nx+1,t), f(0:nx+1,t+1))
+     else if(fds(1).eq.2) then
+        call leapfrog(nx, dt, forcing, f(0:nx+1, t), f(0:nx+1, t-1), f(0:nx+1, t+1))
+     else if(fds(1).ge.3 .and. fds(2).eq.2) then
+        call adams_bashforth(fds(2), nx, dt, forcing, f(0:nx+1,t), f(0:nx+1,t-1), f(0:nx+1,t-1), f(0:nx+1,t-1), f(0:nx+1,t+1))
+     else if(fds(1).ge.3 .and. fds(2).eq.3) then
+        call adams_bashforth(fds(2), nx, dt, forcing, f(0:nx+1,t), f(0:nx+1,t-1), f(0:nx+1,t-2), f(0:nx+1,t-2), f(0:nx+1,t+1))
+     else if(fds(1).ge.3 .and. fds(2).eq.4) then
+        call adams_bashforth(fds(2), nx, dt, forcing, f(0:nx+1,t), f(0:nx+1,t-1), f(0:nx+1,t-2), f(0:nx+1,t-3), f(0:nx+1,t+1))
+     else if(fds(1).ge.6 .and. fds(1).le.8) then
+!        call runge_kutta(fds(1), nx, dt, forcing, f(0:nx+1,t), f(0:nx+1,t-1), f(0:nx+1,t-2), f(0:nx+1,t-3), f(0:nx+1,t+1))
      end if
 
   end do
@@ -81,7 +89,7 @@ contains
     f(nx+1) = f(1)
   end subroutine set_boundary_condition
 
-  subroutine forward_euler_scheme(nx, dt, forcing, f, g)
+  subroutine forward_euler(nx, dt, forcing, f, g)
     implicit none
     integer, intent(in) :: nx
     real,    intent(in) :: dt
@@ -92,9 +100,9 @@ contains
     do x = 1, nx
        g(x) = f(x) + ( ( f(x+1)-f(x-2) ) * f(x-1)  - f(x) + forcing ) * dt
     end do
-  end subroutine forward_euler_scheme
+  end subroutine forward_euler
 
-  subroutine centered_scheme(nx, dt, forcing, f0, f1, g)
+  subroutine leapfrog(nx, dt, forcing, f0, f1, g)
     implicit none
     integer, intent(in) :: nx
     real,    intent(in) :: dt
@@ -106,7 +114,61 @@ contains
     do x = 1, nx
        g(x) = f1(x) + ( ( f0(x+1)-f0(x-2) ) * f0(x-1)  - f0(x) + forcing ) * 2.0d0 * dt
     end do
-  end subroutine centered_scheme
+  end subroutine leapfrog
+
+  subroutine adams_bashforth(dim, nx, dt, forcing, f0, f1, f2, f3, g)
+    implicit none
+    integer, intent(in) :: dim
+    integer, intent(in) :: nx
+    real,    intent(in) :: dt
+    real,    intent(in) :: forcing
+    real, dimension(0:nx+1), intent(in)  :: f0 !(t=t)
+    real, dimension(0:nx+1), intent(in)  :: f1 !(t=t-1)
+    real, dimension(0:nx+1), intent(in)  :: f2 !(t=t-2)
+    real, dimension(0:nx+1), intent(in)  :: f3 !(t=t-3)
+    real, dimension(0:nx+1), intent(out) :: g  !(t=t+1)
+    real, dimension(0:3) :: a
+    integer :: x
+    if(dim.eq.2) then
+       a = (/ 1.5d0, -0.5d0, 0.0d0, 0.0d0 /)
+    else if(dim.eq.3) then
+       a = (/ 23.0d0/12.0d0, -4.0d0/3.0d0, 5.0d0/12.0d0, 0.0d0 /)
+    else if(dim.eq.4) then
+       a = (/ 55.0d0/24.0d0, -59.0d0/24.0d0, 37.0d0/24.0d0, -9.0d0/24.0d0 /)
+    end if
+    do x = 1, nx
+       g(x) = f0(x) + dt * ( a(0) * ( f0(x+1)-f0(x-2) ) * f0(x-1)  - f0(x) + forcing &
+                         & + a(1) * ( f1(x+1)-f1(x-2) ) * f1(x-1)  - f1(x) + forcing &
+                         & + a(2) * ( f2(x+1)-f2(x-2) ) * f2(x-1)  - f2(x) + forcing &
+                         & + a(3) * ( f3(x+1)-f3(x-2) ) * f3(x-1)  - f3(x) + forcing )
+    end do
+  end subroutine adams_bashforth
+
+  subroutine runge_kutta(dim, nx, dt, forcing, f, g)
+    implicit none
+    integer, intent(in) :: dim
+    integer, intent(in) :: nx
+    real,    intent(in) :: dt
+    real,    intent(in) :: forcing
+    real, dimension(0:nx+1), intent(in)  :: f !(t=t)
+    real, dimension(0:nx+1), intent(out) :: g  !(t=t+1)
+    real, dimension(1:4) :: a
+    real    :: k1, k2, k3, k4
+    integer :: x
+    if(dim.eq.2) then
+       a = (/ 1.5d0, -0.5d0, 0.0d0, 0.0d0 /)
+    else if(dim.eq.3) then
+       a = (/ 23.0d0/12.0d0, -4.0d0/3.0d0, 5.0d0/12.0d0, 0.0d0 /)
+    else if(dim.eq.4) then
+       a = (/ 55.0d0/24.0d0, -59.0d0/24.0d0, 37.0d0/24.0d0, -9.0d0/24.0d0 /)
+    end if
+    do x = 1, nx
+!       g(x) = f0(x) + dt * ( a(0) * ( f0(x+1)-f0(x-2) ) * f0(x-1)  - f0(x) + forcing &
+!                         & + a(1) * ( f1(x+1)-f1(x-2) ) * f1(x-1)  - f1(x) + forcing &
+!                         & + a(2) * ( f2(x+1)-f2(x-2) ) * f2(x-1)  - f2(x) + forcing &
+!                         & + a(3) * ( f3(x+1)-f3(x-2) ) * f3(x-1)  - f3(x) + forcing )
+    end do
+  end subroutine runge_kutta
 
   subroutine make_ctl_file(nx, nta)
     implicit none
