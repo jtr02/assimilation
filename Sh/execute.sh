124022238879e4ed568e19d05168e81b70bc1cb2
jtr <jtr.1990.moff@gmail.com>
2018-09-22 19:02:09 +0900

    renew execute.sh

--- Sh/execute.sh
+++ Sh/execute.sh
@@ -1,16 +1,48 @@
 #!/bin/sh
 
+# setting for calculation
+# model
+nx=40
+nt=15000
+dt=0.01d0
+forcing=16.0d0
+# option
+imem=1
+pmem=1
+ptb=0.01d0
+
+# path to directory
 pdir=/home/jtr/Assimilation
+wdir=${pdir}/Work
+sdir=${pdir}/Module/Src
+edir=${pdir}/Module/Exe
 ddir=/mnt/d/Data/Assimilation
 
-gfortran ${pdir}/Module/Src/lorenz96.f90 -o ${pdir}/Module/Exe/lorenz96.out
+# compile
+gfortran ${sdir}/lorenz96.f90 -o ${edir}/lorenz96.out
 
-cd ${pdir}/Work
+# execution start
+cd ${wdir}
 
-${pdir}/Module/Exe/lorenz96.out
-#rm ${pdir}/Module/Exe/lorenz96.out
+# make namelist
+cat << EOF > namelist
+&model
+nx=${nx}
+nt=${nt}
+dt=${dt}
+forcing=${forcing}
+/
+&option
+imem=${imem}
+pmem=${pmem}
+ptb=${ptb}
+/
+EOF
+
+${edir}/lorenz96.out
 
 mv output.grd ${ddir}/Result/
 mv out.ctl ${ddir}/Result/
 
+# execution end
 cd ${pdir}
