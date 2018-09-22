73b9e07714b822ee8d45312fe7582c2a85bccfda
jtr <jtr.1990.moff@gmail.com>
2018-09-22 19:00:46 +0900

    renew lorenz96.f90

--- Module/Src/lorenz96.f90
+++ Module/Src/lorenz96.f90
@@ -1,31 +1,36 @@
 program lorenz96
   implicit none
 
-  ! for integration
-  integer, parameter :: nx = 40
-  integer, parameter :: nt = 5000
-  real, parameter    :: dt = 0.01d0
-  real, parameter    :: forcing = 8.0d0
-  real, parameter    :: ptb = 0.01d0
+  ! for namelist
+  ! unit
+  integer, parameter :: unm = 11
+  ! model
+  integer :: nx
+  integer :: nt
+  double precision :: dt
+  double precision :: forcing
+  ! option
+  integer :: imem
+  integer :: pmem
+  double precision :: ptb
 
-  ! finite difference scheme
-  ! forward-euler_1   : 1,1
-  ! leapfrog_2        : 2,2
-  ! adams-bashforth_2 : 3,2
-  ! adams-bashforth_3 : 4,3
-  ! adams-bashforth_4 : 5.4
-  ! runge-kutta_2     : 6,2
-  ! runge-kutta_3     : 7,3
-  ! runge-kutta_4     : 8,4
-  integer, parameter :: fds(2) = (/ 1 , 1 /)
+  ! for output
+  integer, parameter :: uop = 51
 
   ! variable
-  real, allocatable :: f(:, :)
+  double precision, allocatable :: f(:,:)
 
   ! etc
   real(kind=16) :: st, et
   integer :: x, t
   integer :: i, j, k
+
+  namelist/model/nx, nt, dt, forcing
+  namelist/option/imem, pmem, ptb 
+  open(unit=unm, file='namelist')
+  read(unm, nml=model)
+  read(unm, nml=option)
+  close(unm)
 
   write(*,*) 'this program calculate Lorenz96 model integration'
   write(*,*) 'grid number =      ',nx
@@ -33,52 +38,38 @@ program lorenz96
   write(*,*) 'time step   =      ',dt
   write(*,*) 'forcing     =      ',forcing
   write(*,*) 'perturbation =     ',ptb
-  write(*,*) 'finite difference scheme = ',fds(1)
-  write(*,*) '                  order  = ',fds(2)
+  write(*,*) 'finite difference scheme = runge-kutta'
+  write(*,*) '                  order  = 4th'
 
-  allocate(f(nx+2, nt+1))
-
-  ! initial condition
-  f(:,:) = 0.0d0
-  f(1:nx,1) = forcing
-  call add_random_ptb(nx, ptb, f(1:nx,1))
+  allocate(f(nx+4, nt+1))
 
   ! integration start
-
   call cpu_time(st)
 
-  do t = 1, nt
-
-     call set_boundary_condition(nx, f(0:nx+1,t))
-
-     if(fds(1).eq.1) then
-        call forward_euler(nx, dt, forcing, f(0:nx+1,t), f(0:nx+1,t+1))
-     else if(t.lt.fds(2)) then
-        call forward_euler(nx, dt, forcing, f(0:nx+1,t), f(0:nx+1,t+1))
-     else if(fds(1).eq.2) then
-        call leapfrog(nx, dt, forcing, f(0:nx+1, t), f(0:nx+1, t-1), f(0:nx+1, t+1))
-     else if(fds(1).ge.3 .and. fds(2).eq.2) then
-        call adams_bashforth(fds(2), nx, dt, forcing, f(0:nx+1,t), f(0:nx+1,t-1), f(0:nx+1,t-1), f(0:nx+1,t-1), f(0:nx+1,t+1))
-     else if(fds(1).ge.3 .and. fds(2).eq.3) then
-        call adams_bashforth(fds(2), nx, dt, forcing, f(0:nx+1,t), f(0:nx+1,t-1), f(0:nx+1,t-2), f(0:nx+1,t-2), f(0:nx+1,t+1))
-     else if(fds(1).ge.3 .and. fds(2).eq.4) then
-        call adams_bashforth(fds(2), nx, dt, forcing, f(0:nx+1,t), f(0:nx+1,t-1), f(0:nx+1,t-2), f(0:nx+1,t-3), f(0:nx+1,t+1))
-     else if(fds(1).ge.6 .and. fds(1).le.8) then
-        call runge_kutta(fds(2), nx, dt, forcing, f(0:nx+1,t), f(0:nx+1,t+1))
-     end if
-
+  ! loop of initial condition
+  do i = 1, imem
+     f(:,:) = 0.0d0
+     f(1:nx,1) = forcing * i / imem
+     ! loop of perturbation
+     do j = 1, pmem
+        call add_random_ptb(nx, ptb, f(1:nx,1))
+        ! loop of time
+        do t = 1, nt
+           call set_boundary_condition(nx, f(-1:nx+2,t))
+           call runge_kutta_4(nx, dt, forcing, f(-1:nx+2,t), f(-1:nx+2,t+1))
+        end do
+     end do
   end do
 
   call cpu_time(et)
-
   ! integration end
 
   ! output
-  open(unit=21,file="output.grd", access='direct',form='unformatted',convert='big_endian',recl=nx*4)
+  open(unit=uop,file="output.grd", access='direct',form='unformatted',convert='big_endian',recl=nx*4)
   do t = 1, nt+1
-     write(21,rec=t) f(1:nx, t)
+     write(uop,rec=t) real(f(1:nx, t))
   end do
-  close(21)
+  close(uop)
   call make_ctl_file(nx, nt+1)
 
   write(*,*) 'time for calculation = ',et-st,'seconds'
@@ -86,108 +77,58 @@ program lorenz96
 contains
   subroutine add_random_ptb(nx, norm, f)
     implicit none
-    integer, intent(in)      :: nx
-    real   , intent(in)      :: norm
-    real   , dimension(1:nx), intent(inout) :: f
-    real   , dimension(1:nx) :: rnd
-    call random_number(rnd)
+    integer, intent(in)          :: nx
+    double precision, intent(in) :: norm
+    double precision, dimension(1:nx), intent(inout) :: f
+    integer :: c, seedsize
+    integer, allocatable :: seed(:)
+    double precision, dimension(1:nx) :: rnd
+    call system_clock(count=c)
+    call random_seed(size=seedsize)
+    allocate(seed(seedsize))
+    seed(:) = c
+    call random_seed(put=seed(:))
+    call random_number(rnd(:))
     f(1:nx) = f(1:nx) + (-1.0d0 + rnd(1:nx) * 2.0d0) * f(1:nx) * norm
   end subroutine add_random_ptb
 
   subroutine set_boundary_condition(nx, f)
     implicit none
     integer, intent(in) :: nx
-    real, dimension(0:nx+1), intent(inout) :: f
+    double precision, dimension(-1:nx+2), intent(inout) :: f
     integer :: i
+    f(-1) = f(nx-1)
     f(0) = f(nx)
     f(nx+1) = f(1)
+    f(nx+2) = f(2)
   end subroutine set_boundary_condition
 
-  subroutine forward_euler(nx, dt, forcing, f, g)
+  subroutine runge_kutta_4(nx, dt, forcing, f, g)
     implicit none
     integer, intent(in) :: nx
-    real,    intent(in) :: dt
-    real,    intent(in) :: forcing
-    real, dimension(0:nx+1), intent(in)  :: f !(t=t)
-    real, dimension(0:nx+1), intent(out) :: g !(t=t+1)
-    integer :: x
-    do x = 1, nx
-       g(x) = f(x) + ( ( f(x+1)-f(x-2) ) * f(x-1)  - f(x) + forcing ) * dt
-    end do
-  end subroutine forward_euler
+    double precision,    intent(in) :: dt
+    double precision,    intent(in) :: forcing
+    double precision, dimension(-1:nx+2), intent(in)  :: f !(t=t)
+    double precision, dimension(-1:nx+2), intent(out) :: g !(t=t+1)
+    double precision, dimension(1:nx)    :: k1, k2, k3, k4
+    double precision, dimension(-1:nx+2) :: w
+    k1(1:nx) = ( f(2:nx+1)-f(-1:nx-2) ) * f(0:nx-1) - f(1:nx) + forcing
+    w(1:nx) = f(1:nx) + 0.5d0 * dt * k1(1:nx)
+    call set_boundary_condition(nx, w(-1:nx+2))
+    k2(1:nx) = ( w(2:nx+1)-w(-1:nx-2) ) * w(0:nx-1) - w(1:nx) + forcing
+    w(1:nx) = f(1:nx) + 0.5d0 * dt * k2(1:nx)
+    call set_boundary_condition(nx, w(-1:nx+2))
+    k3(1:nx) = ( w(2:nx+1)-w(-1:nx-2) ) * w(0:nx-1) - w(1:nx) + forcing
+    w(1:nx) = f(1:nx) + dt * k3(1:nx)
+    call set_boundary_condition(nx, w(-1:nx+2))
+    k4(1:nx) = ( w(2:nx+1)-w(-1:nx-2) ) * w(0:nx-1) - w(1:nx) + forcing
+    g(1:nx) = f(1:nx) + ( k1(1:nx) + k2(1:nx)*2.0d0 + k3(1:nx)*2.0d0 + k4(1:nx) ) * dt / 6.0d0
+  end subroutine runge_kutta_4
 
-  subroutine leapfrog(nx, dt, forcing, f0, f1, g)
+  subroutine make_ctl_file(nx, nt)
     implicit none
     integer, intent(in) :: nx
-    real,    intent(in) :: dt
-    real,    intent(in) :: forcing
-    real, dimension(0:nx+1), intent(in)  :: f0 !(t=t)
-    real, dimension(0:nx+1), intent(in)  :: f1 !(t=t-1)
-    real, dimension(0:nx+1), intent(out) :: g  !(t=t+1)
-    integer :: x
-    do x = 1, nx
-       g(x) = f1(x) + ( ( f0(x+1)-f0(x-2) ) * f0(x-1)  - f0(x) + forcing ) * 2.0d0 * dt
-    end do
-  end subroutine leapfrog
-
-  subroutine adams_bashforth(order, nx, dt, forcing, f0, f1, f2, f3, g)
-    implicit none
-    integer, intent(in) :: order
-    integer, intent(in) :: nx
-    real,    intent(in) :: dt
-    real,    intent(in) :: forcing
-    real, dimension(0:nx+1), intent(in)  :: f0 !(t=t)
-    real, dimension(0:nx+1), intent(in)  :: f1 !(t=t-1)
-    real, dimension(0:nx+1), intent(in)  :: f2 !(t=t-2)
-    real, dimension(0:nx+1), intent(in)  :: f3 !(t=t-3)
-    real, dimension(0:nx+1), intent(out) :: g  !(t=t+1)
-    real, dimension(0:3) :: a
-    integer :: x
-    if(order.eq.2) then
-       a = (/ 1.5d0, -0.5d0, 0.0d0, 0.0d0 /)
-    else if(order.eq.3) then
-       a = (/ 23.0d0/12.0d0, -4.0d0/3.0d0, 5.0d0/12.0d0, 0.0d0 /)
-    else if(order.eq.4) then
-       a = (/ 55.0d0/24.0d0, -59.0d0/24.0d0, 37.0d0/24.0d0, -9.0d0/24.0d0 /)
-    end if
-    do x = 1, nx
-       g(x) = f0(x) + dt * ( a(0) * ( f0(x+1)-f0(x-2) ) * f0(x-1)  - f0(x) + forcing &
-                         & + a(1) * ( f1(x+1)-f1(x-2) ) * f1(x-1)  - f1(x) + forcing &
-                         & + a(2) * ( f2(x+1)-f2(x-2) ) * f2(x-1)  - f2(x) + forcing &
-                         & + a(3) * ( f3(x+1)-f3(x-2) ) * f3(x-1)  - f3(x) + forcing )
-    end do
-  end subroutine adams_bashforth
-
-  subroutine runge_kutta(order, nx, dt, forcing, f, g)
-    implicit none
-    integer, intent(in) :: order
-    integer, intent(in) :: nx
-    real,    intent(in) :: dt
-    real,    intent(in) :: forcing
-    real, dimension(0:nx+1), intent(in)  :: f !(t=t)
-    real, dimension(0:nx+1), intent(out) :: g !(t=t+1)
-    real, dimension(1:4) :: a
-    real    :: k1, k2, k3, k4
-    integer :: x
-    if(order.eq.2) then
-       a = (/ 1.0d0, 0.5d0, 0.0d0, 0.0d0 /)
-    else if(order.eq.3) then
-       a = (/ 1.0d0/3.0d0, 15.0d0/16.0d0, 8.0d0/15.0d0, 0.0d0 /)
-    else if(order.eq.4) then
-       a = (/ 1.0d0/6.0d0, 2.0d0/6.0d0, 2.0d0/6.0d0, 1.0d0/6.0d0 /)
-    end if
-    do x = 1, nx
-!       g(x) = f0(x) + dt * ( a(0) * ( f0(x+1)-f0(x-2) ) * f0(x-1)  - f0(x) + forcing &
-!                         & + a(1) * ( f1(x+1)-f1(x-2) ) * f1(x-1)  - f1(x) + forcing &
-!                         & + a(2) * ( f2(x+1)-f2(x-2) ) * f2(x-1)  - f2(x) + forcing &
-!                         & + a(3) * ( f3(x+1)-f3(x-2) ) * f3(x-1)  - f3(x) + forcing )
-    end do
-  end subroutine runge_kutta
-
-  subroutine make_ctl_file(nx, nta)
-    implicit none
-    integer, intent(in) :: nx
-    integer, intent(in) :: nta
+    integer, intent(in) :: nt
     open(unit=31,file="out.ctl")
     write(31,*) 'dset ^output.grd'
     write(31,*)	'options big_endian'
@@ -195,7 +136,7 @@ contains
     write(31,*)	'xdef ',nx,' linear 1 1'
     write(31,*)	'ydef 1 levels 0'
     write(31,*) 'zdef 1 levels 1000'
-    write(31,*) 'tdef ',nta,' linear 00z01jan0000 72mn'
+    write(31,*) 'tdef ',nt,' linear 00z01jan0000 72mn'
     write(31,*) 'vars 1'
     write(31,*) 'f 0 99 variable'
     write(31,*) 'endvars'
