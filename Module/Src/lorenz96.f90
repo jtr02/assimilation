4805709ec21aecb22232a4085e515828f3b51d2d
jtr <jtr.1990.moff@gmail.com>
2018-09-23 08:05:52 +0900

    add calculation of error growth

--- Module/Src/lorenz96.f90
+++ Module/Src/lorenz96.f90
@@ -12,24 +12,29 @@ program lorenz96
   ! option
   integer :: imem
   integer :: pmem
-
-  ! for output
-  integer, parameter :: uop = 51
+  ! unit
+  integer :: ufc
+  integer :: ueg
 
   ! variable
   double precision, allocatable :: f(:,:)
+  double precision :: initial
   double precision :: norm
+  double precision, allocatable :: ravr(:)
 
   ! etc
   real(kind=16) :: st, et
   integer :: x, t
   integer :: i, j, k
+  double precision :: r
 
   namelist/model/nx, nt, dt, forcing
   namelist/option/imem, pmem 
+  namelist/unit/ufc, ueg
   open(unit=unm, file='namelist')
   read(unm, nml=model)
   read(unm, nml=option)
+  read(unm, nml=unit)
   close(unm)
 
   write(*,*) 'this program calculate Lorenz96 model integration'
@@ -41,36 +46,44 @@ program lorenz96
   write(*,*) '                  order  = 4th'
 
   allocate(f(nx+4, nt+1))
+  allocate(ravr(nt))
+  ravr(1:nt) = 0.0d0
 
   ! integration start
   call cpu_time(st)
 
   ! loop of initial condition
   do i = 1, imem
+     initial = forcing * i / imem
      f(:,:) = 0.0d0
-     f(1:nx,1) = forcing * i / imem
+     f(1:nx,1) = initial
      ! loop of perturbation
      do j = 1, pmem
         call add_random_ptb(nx, f(1:nx,1), norm)
-        write(*,*) 'norm = ',norm
         ! loop of time
         do t = 1, nt
            call set_boundary_condition(nx, f(-1:nx+2,t))
            call runge_kutta_4(nx, dt, forcing, f(-1:nx+2,t), f(-1:nx+2,t+1))
+           call calc_error_growth(nx, initial, f(1:nx,t+1), norm, r)
+           ravr(t) = ravr(t) + r
         end do
      end do
   end do
+
+  ! error growth
+  ravr(1:nt) = sqrt( ravr(1:nt) / imem / pmem )
+  ravr(1:nt) = log(ravr(1:nt)) / log(2.0d0)
 
   ! integration end
   call cpu_time(et)
 
   ! output
-  open(unit=uop,file="output.grd", access='direct',form='unformatted',convert='big_endian',recl=nx*4)
-  do t = 1, nt+1
-     write(uop,rec=t) real(f(1:nx, t))
-  end do
-  close(uop)
-  call make_ctl_file(nx, nt+1)
+  ! forecast
+  call output(ufc, nx, nt+1, f(1:nx,1:nt+1))
+  call make_ctl_file(ufc, nx, nt+1)
+  ! error gwowth
+  call output(ueg, 1, nt, ravr(1:nt))
+  call make_ctl_file(ueg, 1, nt)
 
   write(*,*) 'time for calculation = ',et-st,'seconds'
 
@@ -128,12 +141,38 @@ contains
     g(1:nx) = f(1:nx) + ( k1(1:nx) + k2(1:nx)*2.0d0 + k3(1:nx)*2.0d0 + k4(1:nx) ) * dt / 6.0d0
   end subroutine runge_kutta_4
 
-  subroutine make_ctl_file(nx, nt)
-    implicit none
+  subroutine calc_error_growth(nx, initial, f, norm, r)
+    integer, intent(in) :: nx
+    double precision, intent(in)  :: initial
+    double precision, dimension(1:nx), intent(in) :: f !(t=t)
+    double precision, intent(in)  :: norm
+    double precision, intent(out) :: r
+    double precision :: work(nx)
+    work(1:nx) = ( f(1:nx) - initial) * ( f(1:nx) - initial )
+    r = sum(work(1:nx)) / (norm * norm)
+  end subroutine calc_error_growth
+
+  subroutine output(unit, nx, nt, f)
+    integer, intent(in) :: unit
     integer, intent(in) :: nx
     integer, intent(in) :: nt
-    open(unit=31,file="out.ctl")
-    write(31,*) 'dset ^output.grd'
+    double precision, dimension(1:nx,1:nt), intent(in) :: f
+    character(2) :: u
+    write(u,'(i2)') unit
+    open(unit=unit,file='out'//u//'.grd', access='direct',form='unformatted',convert='big_endian',recl=nx*nt*4)
+    write(unit,rec=1) real(f(1:nx,1:nt))
+    close(unit)
+  end subroutine output
+
+  subroutine make_ctl_file(unit, nx, nt)
+    implicit none
+    integer, intent(in) :: unit
+    integer, intent(in) :: nx
+    integer, intent(in) :: nt
+    character(2) :: u
+    write(u,'(i2)') unit
+    open(unit=31,file='out'//u//'.ctl')
+    write(31,*) 'dset ^out',u,'.grd'
     write(31,*)	'options big_endian'
     write(31,*)	'undef -9.99E+33'
     write(31,*)	'xdef ',nx,' linear 1 1'
